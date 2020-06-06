995d68f875aa23a7cd686b6ec03ad35c4693b8a6
Óscar Heredia <65867332+OscHer@users.noreply.github.com>
2020-06-06 18:20:13 +0200

    Base Metapackage added.

--- provision_scripts/config.sh
+++ provision_scripts/config.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # Configuración de metapaquetes a instalar
+METAPAQUETE_BASE=1
 METAPAQUETE_GIT=1
 METAPAQUETE_LAMP=1
 METAPAQUETE_PYTHON=0
@@ -8,6 +9,7 @@ METAPAQUETE_DOCKER=0
 METAPAQUETE_C=0
 
 # Cadenas de instalación de metapaquetes
+CADENA_METAPAQUETE_BASE="vim"
 CADENA_METAPAQUETE_LAMP="apache2 mariadb-server mariadb-common php php-cli libapache2-mod-php php-mysql php-gd php-xml curl lynx"
 CADENA_METAPAQUETE_PYTHON="python3-pip python-dev"
 CADENA_METAPAQUETE_DOCKER=""
