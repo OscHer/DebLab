279fc69ce54afd4aeafcb613b967d48cb5cdefc5
oscar <65867332+OscHer@users.noreply.github.com>
2020-06-06 19:38:45 +0200

    Some packages added to python metapackage

--- provision_scripts/config.sh
+++ provision_scripts/config.sh
@@ -11,6 +11,6 @@ METAPAQUETE_C=0
 # Cadenas de instalación de metapaquetes
 CADENA_METAPAQUETE_BASE="vim"
 CADENA_METAPAQUETE_LAMP="apache2 mariadb-server mariadb-common php php-cli libapache2-mod-php php-mysql php-gd php-xml curl lynx"
-CADENA_METAPAQUETE_PYTHON="python3 python3-pip python-dev virtualenv"
+CADENA_METAPAQUETE_PYTHON="python3 python3-pip python3-dev virtualenv"
 CADENA_METAPAQUETE_DOCKER=""
 CADENA_METAPAQUETE_C="build-essential"
