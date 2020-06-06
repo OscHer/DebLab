10cbcc2bd473ef811f1421a46a9a0884a41564c9
Óscar Heredia <65867332+OscHer@users.noreply.github.com>
2020-06-06 20:25:26 +0200

    Git added to base metapackage

--- provision_scripts/config.sh
+++ provision_scripts/config.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 # Configuración de metapaquetes a instalar
-METAPAQUETE_BASE=0
+METAPAQUETE_BASE=1
 METAPAQUETE_GIT=0
 METAPAQUETE_LAMP=0
-METAPAQUETE_PYTHON=1
+METAPAQUETE_PYTHON=0
 METAPAQUETE_DOCKER=0
 METAPAQUETE_C=0
 
 # Cadenas de instalación de metapaquetes
-CADENA_METAPAQUETE_BASE="vim"
+CADENA_METAPAQUETE_BASE="vim git"
 CADENA_METAPAQUETE_LAMP="apache2 mariadb-server mariadb-common php php-cli libapache2-mod-php php-mysql php-gd php-xml curl lynx"
 CADENA_METAPAQUETE_PYTHON="python3 python3-pip python3-dev virtualenv"
 CADENA_METAPAQUETE_DOCKER=""
