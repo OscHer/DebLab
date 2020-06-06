7c13aee985278c64ca3e8a6e226bcb5aedf8c0c2
oscar <65867332+OscHer@users.noreply.github.com>
2020-06-06 18:55:03 +0200

    Add virtualenv to python Metapackage

--- provision_scripts/config.sh
+++ provision_scripts/config.sh
@@ -4,13 +4,13 @@
 METAPAQUETE_BASE=0
 METAPAQUETE_GIT=0
 METAPAQUETE_LAMP=0
-METAPAQUETE_PYTHON=0
+METAPAQUETE_PYTHON=1
 METAPAQUETE_DOCKER=0
 METAPAQUETE_C=0
 
 # Cadenas de instalación de metapaquetes
 CADENA_METAPAQUETE_BASE="vim"
 CADENA_METAPAQUETE_LAMP="apache2 mariadb-server mariadb-common php php-cli libapache2-mod-php php-mysql php-gd php-xml curl lynx"
-CADENA_METAPAQUETE_PYTHON="python3-pip python-dev"
+CADENA_METAPAQUETE_PYTHON="python3 python3-pip python-dev virtualenv"
 CADENA_METAPAQUETE_DOCKER=""
 CADENA_METAPAQUETE_C="build-essential"
