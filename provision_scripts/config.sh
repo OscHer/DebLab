7daa1ec20e21e00f4c91ba5eadb52796d82636a5
oscar <65867332+OscHer@users.noreply.github.com>
2020-06-13 19:17:00 +0200

    php-curl added to metapackage LAMP

--- provision_scripts/config.sh
+++ provision_scripts/config.sh
@@ -10,7 +10,7 @@ METAPAQUETE_C=0
 
 # Cadenas de instalación de metapaquetes
 CADENA_METAPAQUETE_BASE="vim git curl"
-CADENA_METAPAQUETE_LAMP="apache2 mariadb-server mariadb-common php php-cli libapache2-mod-php php-mysql php-gd php-xml curl lynx php-dev libmcrypt-dev"
+CADENA_METAPAQUETE_LAMP="apache2 mariadb-server mariadb-common php php-cli libapache2-mod-php php-mysql php-gd php-xml curl lynx php-dev libmcrypt-dev php-curl"
 CADENA_METAPAQUETE_PYTHON="python3 python3-pip python3-dev virtualenv"
 CADENA_METAPAQUETE_DOCKER=""
 CADENA_METAPAQUETE_C="build-essential"
