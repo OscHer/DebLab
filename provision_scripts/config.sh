bcd6bc750660ba36d5e5794a6e08ecf4df86c650
oscar <65867332+OscHer@users.noreply.github.com>
2020-06-14 09:30:13 +0200

    Add zip and unzip to Metapackage base

--- provision_scripts/config.sh
+++ provision_scripts/config.sh
@@ -9,7 +9,7 @@ METAPAQUETE_DOCKER=0
 METAPAQUETE_C=0
 
 # Cadenas de instalación de metapaquetes
-CADENA_METAPAQUETE_BASE="vim git curl"
+CADENA_METAPAQUETE_BASE="vim git curl zip unzip"
 CADENA_METAPAQUETE_LAMP="apache2 mariadb-server mariadb-common php php-cli libapache2-mod-php php-mysql php-gd php-xml curl lynx php-dev libmcrypt-dev php-curl php-soap php-zip php-mbstring php-gettext php-seclib php-bz2"
 CADENA_METAPAQUETE_PYTHON="python3 python3-pip python3-dev virtualenv"
 CADENA_METAPAQUETE_DOCKER=""
